655fb4e45273741e4ebd30ddb08ee3e0535630c2
The-Hawkeye <82801422+The-Hawkeye@users.noreply.github.com>
2023-11-21 12:57:50 +0530

    final

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./config/config");
+
+let server;
+
+// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
+
+mongoose.connect(config.mongoose.url, { useUnifiedTopology: true , useNewUrlParser: true }).then(()=>{
+    try {
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(config.port, ()=>{
+    console.log("Server Listening at PORT ",config.port)
+})
